208679f6f0f9d3ff8f7cb8f404f30c638d0d6eee
Pochechuev <trakturus@list.ru>
2023-08-03 22:43:03 +0300

    wrote a programm Задача_2

---
+++ Задача_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int num1 = new Random().Next(0, 100);
+Console.WriteLine("Первое число: " + num1);
+int num2 = new Random().Next(0, 100);
+Console.WriteLine("Второе число: " + num2);
+
+
+if (num1 > num2)
+{
+    Console.WriteLine("Наибольшее из двух чисел " + num1);
+}
+
+else if (num2 > num1)
+{
+    Console.WriteLine("Наибольшее из двух чисел " + num2);
+}
