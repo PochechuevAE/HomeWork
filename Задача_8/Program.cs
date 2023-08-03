ab7f34297da248f8d618f6bb1328b94fbda919e9
Pochechuev <trakturus@list.ru>
2023-08-04 00:23:13 +0300

    write a programm Задача_8 and include -N and 1

---
+++ Задача_8/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+int num = new Random().Next(-100, 100);
+System.Console.WriteLine("Задано число: " + num);
+int numStarts = 1;
+System.Console.WriteLine("Все чётные числа от 1 до " + num + ":");
+if (num > 0)
+{
+    while (numStarts <= num)
+    {
+        if (numStarts % 2 == 0)
+        {
+            System.Console.WriteLine(numStarts);
+        }
+        numStarts++;
+    }
+}
+if (num == 1)
+{
+    System.Console.WriteLine("Нет чётных чисел");
+}
+if (num < 0)
+{
+    while (numStarts >= num)
+    {
+        if (numStarts % 2 == 0)
+        {
+            System.Console.WriteLine(numStarts);
+        }
+        numStarts--;
+    }
+}
