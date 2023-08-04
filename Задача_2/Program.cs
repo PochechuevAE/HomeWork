7156ae173985a451530f2113f79dad31a2d10a9a
Pochechuev <trakturus@list.ru>
2023-08-04 14:29:23 +0300

    write variant 2 for Задача_2

--- Задача_2/Program.cs
+++ Задача_2/Program.cs
@@ -4,18 +4,13 @@ Console.WriteLine("Первое число: " + num1);
 int num2 = new Random().Next(0, 100);
 Console.WriteLine("Второе число: " + num2);
 
-
-if (num1 > num2)
+if (num1 == num2)
 {
-    Console.WriteLine("Наибольшее из двух чисел " + num1 + " Наименьшее из двух чисел " + num2);
+    Console.WriteLine("Числа равны!");
 }
 
-else if (num2 > num1)
+else
 {
-    Console.WriteLine("Наибольшее из двух чисел " + num2 + " Наименьшее из двух чисел " + num1);
-}
-
-else if (num1 == num2)
-{
-    Console.WriteLine("Числа равны " + num1);
+    int max = Math.Max(num1, num2);
+    System.Console.WriteLine("Максимальное число: " + max + " Минимальное число: " + Math.Min(num1,num2));
 }
