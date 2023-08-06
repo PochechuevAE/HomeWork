d359219a310aa41928672b91aab6cbc55f834ebd
Pochechuev <trakturus@list.ru>
2023-08-06 18:39:20 +0300

    change validation message

--- Seminar2/Задача15/Program.cs
+++ Seminar2/Задача15/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 int TakeNumberOfDay(int numberday)
 {
-    System.Console.WriteLine("Введите день недели номер дня недели от 1 до 7: ");
+    System.Console.WriteLine("Введите номер дня недели от 1 до 7: ");
     int numberofday = int.Parse(Console.ReadLine());
     return numberofday;
 }
@@ -27,7 +27,7 @@ bool ValidateMessage(int number)
     }
     else
     {
-        System.Console.WriteLine("Введите номер дня недели от 1 до 7!!!");
+        System.Console.WriteLine("Введен некорректный номер дня недели. Повторите ввод от 1 до 7!!!");
         return false;
     }
 }
