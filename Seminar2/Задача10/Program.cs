43166162d982f93312c75f5a09e050225b51baae
Pochechuev <trakturus@list.ru>
2023-08-06 17:40:55 +0300

    write a program Задача10

---
+++ Seminar2/Задача10/Program.cs
@@ -0,0 +1,5 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int num = new Random().Next(100, 1000);
+System.Console.WriteLine($"Выбрано число: {num}");
+int secondDigit = num / 10 % 10;
+System.Console.WriteLine($"Вторая цифра числа: {secondDigit}");
