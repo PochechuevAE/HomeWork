85ac528f954fda2d2de62a674405e873bc71d3b0
Pochechuev <trakturus@list.ru>
2023-08-06 18:12:19 +0300

    write program Задача13

---
+++ Seminar2/Задача13/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int TakeNumber(int startnum)
+{
+    System.Console.WriteLine("Введите число: ");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+bool ValidateMessage(int number)
+{
+    if (number < 100)
+    {
+        System.Console.WriteLine("Третьей цифры нет! Повторите ввод!");
+        return false;
+    }
+    else 
+    {
+        return true;
+    }
+}
+
+int GetThirdDigit(int number)
+{
+    while (number > 999) 
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+int number = TakeNumber(100);
+if (ValidateMessage(number))
+{
+    System.Console.WriteLine(GetThirdDigit(number));
+}
