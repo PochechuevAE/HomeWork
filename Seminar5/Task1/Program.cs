c467ce5740eeb5ad2b3944ccceb34e3969c0da35
Pochechuev <trakturus@list.ru>
2023-09-04 15:00:10 +0300

    Wrote task1 in Seminar_5

--- Seminar5/Task1/Program.cs
+++ Seminar5/Task1/Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int [] arr = GenerateArray(5);
+System.Console.WriteLine("Выбран массив:");
+PrintArray(arr);
+System.Console.WriteLine($"Кол-во чётных чисел в массиве: {CountEvenNumbrs(arr)}");
+
+int CountEvenNumbrs(int[] array)
+{
+    int count = default;
+
+    for(int i = 0; i < array.Length; i++) 
+    {
+        if(array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int [] GenerateArray(int Length) 
+{
+    int [] result = new int[Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(100, 1000);
+    }
+
+    return result;
+} 
+
+void PrintArray(int [] array)
+{
+    foreach (int i in array)
+    {
+        System.Console.Write($"{i} ");
+    }
+    System.Console.WriteLine();
+}
