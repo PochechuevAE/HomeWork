43361207beb34fdce1e6fa792dd2d980c81c050f
PochechuevAE <140548126+PochechuevAE@users.noreply.github.com>
2023-09-14 22:11:09 +0300

    Update Program.cs

--- attistat/task3/Program.cs
+++ attistat/task3/Program.cs
@@ -11,18 +11,18 @@ int n = SetNumber("второе");
 
 int functionAkkerman = Ack(m, n);
 
-Console.Write($"Функция Аккермана = {functionAkkerman} ");
+Console.Write($"Функция Аккермана A({m},{n}) = {functionAkkerman} ");
 
-int Ack(int m, int n)
+
+int SetNumber(string message = "") 
+ { 
+     System.Console.Write($"Введите {message} число  : "); 
+     return Convert.ToInt32(System.Console.ReadLine()); 
+ }
+
+int Akkerman(int m, int n)
 {
   if (m == 0) return n + 1;
-  else if (n == 0) return Ack(m - 1, 1);
-  else return Ack(m - 1, Ack(m, n - 1));
+  else if (n == 0) return Akkerman(m - 1, 1);
+  else return Akkerman(m - 1, Akkerman(m, n - 1));
 }
-
-int InputNumbers(string input) 
-{
-  Console.Write(input);
-  int output = Convert.ToInt32(Console.ReadLine());
-  return output;
-}﻿
