cf9391733b593185872ef2190cc45836bff1e081
Pochechuev <trakturus@list.ru>
2023-09-14 20:11:30 +0300

    wrote task2

--- attistat/task2/Program.cs
+++ attistat/task2/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿/*
+ Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int startNumber = SetNumber("первое");
+int endNumber = SetNumber("второе");
+System.Console.WriteLine($"Сумма натуральных элементов в промежутке от {startNumber} до {endNumber} равна: ");
+System.Console.WriteLine($"{PrintNumbers(startNumber, endNumber)}\t");
+
+int SetNumber(string message = "")
+{
+    System.Console.Write($"Введите {message} число  : ");
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+
+
+int PrintNumbers(int startNumber, int endNumber)
+{
+    int sum = default;
+    if (startNumber == endNumber) 
+    {
+        return startNumber;
+    }
+   
+    sum = startNumber + PrintNumbers(startNumber + 1, endNumber);
+    return sum;
+    
+}
