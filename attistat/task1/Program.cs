6d978d13aa6cf2a0b47648dab4e7c3f22c9575b9
Pochechuev <trakturus@list.ru>
2023-09-14 19:41:43 +0300

    wrote task1

--- attistat/task1/Program.cs
+++ attistat/task1/Program.cs
@@ -3,4 +3,22 @@
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
+int endNumber = SetNumber("иниересующее Вас");
+System.Console.WriteLine($"Натуральные числа в промежутке от {endNumber} до 1: ");
+System.Console.WriteLine($"{PrintNumbers(1, endNumber)}\t");
 
+int SetNumber(string message = "")
+{
+    System.Console.Write($"Введите {message} число  : ");
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+
+string PrintNumbers(int startNumber, int endNumber)
+{
+    if (startNumber == endNumber) 
+    {
+        return startNumber.ToString();
+    }
+    
+    return endNumber + " " + PrintNumbers(startNumber, endNumber - 1);
+}
